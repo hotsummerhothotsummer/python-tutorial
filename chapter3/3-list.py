39ca2050f1302124c18ec3c241420599646c6cf0
gta <hongsm4308@gmail.com>
2023-09-02 10:55:30 +0000

    list and type of list

---
+++ chapter3/3-list.py
@@ -0,0 +1,8 @@
+my_list = [1, 2, 3, 4, 5]
+my_list.append(999999)
+print(my_list)
+print(type(my_list))
+my_list1 = ["ghdtjdals", "ghdsdjdj", "fjhdjfhdj"]
+my_list1.sort()
+print(my_list1)
+print(type(my_list1))
