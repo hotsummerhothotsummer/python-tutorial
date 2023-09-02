c2b95ac34e6b1d09a094501b7737c28a13286b02
gta <hongsm4308@gmail.com>
2023-09-02 11:33:36 +0000

    string

--- chapter3/3-string.py
+++ chapter3/3-string.py
@@ -5,3 +5,8 @@ print(type(a))
 b = "my birthday is 911"
 print(b)
 print(b[::3])
+print(b[1:2:-1])
+number = int(input("put integral:"))
+days=int(input("put integral:"))
+c="i had %d apples so i got bellyache for %d days" %(number,days)
+print(c)
