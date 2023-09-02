1ebcb156b40bbaa9bdf64a006f0bc0f64644592c
gta <hongsm4308@gmail.com>
2023-09-02 11:00:10 +0000

    list again

--- chapter3/3-list.py
+++ chapter3/3-list.py
@@ -6,3 +6,8 @@ my_list1 = ["ghdtjdals", "ghdsdjdj", "fjhdjfhdj"]
 my_list1.sort()
 print(my_list1)
 print(type(my_list1))
+my_list3 = [1,2,3,["a", "b", "c" ]]
+my_list3.append([5,6,7])
+print(my_list3)
+print(my_list3[-1][-3])
+print(type(my_list3[-1][-3]))
