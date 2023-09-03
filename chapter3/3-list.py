24a298cbedadebf5143872f753d6a805aec774ed
gta <hongsm4308@gmail.com>
2023-09-03 10:49:22 +0000

    string

--- chapter3/3-list.py
+++ chapter3/3-list.py
@@ -5,9 +5,15 @@ print(type(my_list))
 my_list1 = ["ghdtjdals", "ghdsdjdj", "fjhdjfhdj"]
 my_list1.sort()
 print(my_list1)
-print(type(my_list1))
-my_list3 = [1,2,3,["a", "b", "c" ]]
-my_list3.append([5,6,7])
+print(type(my_list1[0]))
+my_list3 = [1, 2, 3, ["a", "b", "c"]]
+my_list3.append([5, 6, 7])
 print(my_list3)
 print(my_list3[-1][-3])
 print(type(my_list3[-1][-3]))
+a = [1, 2, 3, 4, 5]
+b = [4, 5, 6]
+print(a+b)
+a[0] = "ripple"
+print(a+b)
+a.sort()
